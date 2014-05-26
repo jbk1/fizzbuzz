cd127cc360bebe3aae2003b436ca27a85f16aebb
James B Kemp <james@bibble.com>
2014-05-26 20:57:47 +0100

    fizzbuzz app file ready for take2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,24 +1,29 @@
-def fizzbuzz(number)
-return "FizzBuzz" if is_divisible_by_fifteen?(number) 
-return 'Buzz' if is_divisible_by_five?(number)
-return 'Fizz' if is_divisible_by_three?(number)
-number
-end
 
-def is_divisible_by_three?(number)
-	is_divisible_by?(number, 3)
-end
 
-def is_divisible_by_five?(number)
-	is_divisible_by?(number, 5)
-end
 
-def is_divisible_by_fifteen?(number)
-	is_divisible_by?(number, 15)
-end
+# =========================================
 
-def is_divisible_by?(number, divisor)
-	number % divisor == 0
-end
+# def fizzbuzz(number)
+# return "FizzBuzz" if is_divisible_by_fifteen?(number) 
+# return 'Buzz' if is_divisible_by_five?(number)
+# return 'Fizz' if is_divisible_by_three?(number)
+# number
+# end
+
+# def is_divisible_by_three?(number)
+# 	is_divisible_by?(number, 3)
+# end
+
+# def is_divisible_by_five?(number)
+# 	is_divisible_by?(number, 5)
+# end
+
+# def is_divisible_by_fifteen?(number)
+# 	is_divisible_by?(number, 15)
+# end
+
+# def is_divisible_by?(number, divisor)
+# 	number % divisor == 0
+# end
 
 # just a comment here to test git pull origin master.
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,53 +1,57 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-	context 'knows that a number is divisible by' do
-		it '3' do
-			expect(is_divisible_by_three?(3)).to be_true
-		end
-	end
 
-	context 'knows that a number is divisible by' do
-		it '5' do
-			expect(is_divisible_by_five?(5)).to be_true
-		end
-	end
+# =======================================
+# below was my first attempy, now i'll try my second attempt, but on the same file on a second branch on the repo 'fizzbuzz', branch name 'fzbbr2'
 
-	context 'knows that a number is divisible by' do
-		it '15' do
-			expect(is_divisible_by_fifteen?(15)).to be_true
-		end
-	end
+# describe 'fizzbuzz' do
+# 	context 'knows that a number is divisible by' do
+# 		it '3' do
+# 			expect(is_divisible_by_three?(3)).to be_true
+# 		end
+# 	end
 
-	context 'knows that a number is not divisible by' do 
-			it '3' do
-			expect(is_divisible_by_three?(1)).not_to be_true
-		end
-	end
+# 	context 'knows that a number is divisible by' do
+# 		it '5' do
+# 			expect(is_divisible_by_five?(5)).to be_true
+# 		end
+# 	end
+
+# 	context 'knows that a number is divisible by' do
+# 		it '15' do
+# 			expect(is_divisible_by_fifteen?(15)).to be_true
+# 		end
+# 	end
+
+# 	context 'knows that a number is not divisible by' do 
+# 			it '3' do
+# 			expect(is_divisible_by_three?(1)).not_to be_true
+# 		end
+# 	end
 	
-	context 'knows that a number is not divisible by' do
-		it '5' do
-			expect(is_divisible_by_five?(1)).not_to be_true
-		end
-	end
+# 	context 'knows that a number is not divisible by' do
+# 		it '5' do
+# 			expect(is_divisible_by_five?(1)).not_to be_true
+# 		end
+# 	end
 
-	context 'knows that a number is not divisible by' do
-		it '15' do
-			expect(is_divisible_by_fifteen?(1)).not_to be_true
-		end
-	end
+# 	context 'knows that a number is not divisible by' do
+# 		it '15' do
+# 			expect(is_divisible_by_fifteen?(1)).not_to be_true
+# 		end
+# 	end
 	
-		context 'while playing the game it returns' 
-			it 'the number' do
-			expect(fizzbuzz(1)).to eq 1
-			end
-			it 'Fizz' do
-				expect(fizzbuzz(3)).to eq 'Fizz'
-			end
-			it 'Buzz' do
-					expect(fizzbuzz(5)).to eq 'Buzz'
-			end
-			it 'FizzBuzz' do
-					expect(fizzbuzz(15)).to eq 'FizzBuzz'
-			end
-end
+# 		context 'while playing the game it returns' 
+# 			it 'the number' do
+# 			expect(fizzbuzz(1)).to eq 1
+# 			end
+# 			it 'Fizz' do
+# 				expect(fizzbuzz(3)).to eq 'Fizz'
+# 			end
+# 			it 'Buzz' do
+# 					expect(fizzbuzz(5)).to eq 'Buzz'
+# 			end
+# 			it 'FizzBuzz' do
+# 					expect(fizzbuzz(15)).to eq 'FizzBuzz'
+# 			end
+# end
