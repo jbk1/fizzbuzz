f5a2ee1e6197a4156b917f15bfda152ea24e9f22
James B Kemp <james@bibble.com>
2014-05-26 22:09:08 +0100

    completed FizzBuzz for 2nd time

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
 
-	context "knows that a number is divisible by"
+	context "knows that a number is divisible by" do
 
 		it '3' do
 			expect(is_divisible_by_three?(3)).to be_true
@@ -15,9 +15,10 @@ describe 'Fizzbuzz' do
 		it '15' do
 			expect(is_divisible_by_fifteen?(15)).to be_true
 		end
+	end
 
 
-	context "knows that a number is not divisible by"
+	context "knows that a number is not divisible by" do
 		
 		it '3' do
 			expect(is_divisible_by_three?(1)).not_to be_true
@@ -30,7 +31,27 @@ describe 'Fizzbuzz' do
 		it '15' do
 			expect(is_divisible_by_fifteen?(4)).not_to be_true
 		end
+	end
 
+	context "while playing the game it returns" do
+
+		it 'the number' do
+			expect(fizzbuzz(1)).to eq 1
+		end
+
+		it 'Fizz' do
+			expect(fizzbuzz(3)).to eq 'Fizz'
+		end
+
+		it 'Buzz' do
+			expect(fizzbuzz(5)).to eq 'Buzz'
+		end
+
+		it 'FizzBuzz' do
+			expect(fizzbuzz(15)).to eq 'FizzBuzz'
+		end
+	
+	end
 
 
 end
